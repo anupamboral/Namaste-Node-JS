f50848782e319a3e89822dcbd22cccd2c67b7178
anupamboral <anupamboral6889@gmail.com>
2025-07-21 11:54:22 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+var a = 10;
+var b = 20;
+
+console.log(a + b);
+//*to run app.js then in the terminal we just need to write "node app.js" then enter and it will execute our code. As Node.js already contains V8 inside it.
