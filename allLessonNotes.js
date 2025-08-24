7cc26d371fce486e1ebee06fbd84c4df635e887d
anupamboral <anupamboral6889@gmail.com>
2025-08-24 15:02:58 +0530

    server - client architecture

--- allLessonNotes.js
+++ allLessonNotes.js
@@ -731,3 +731,54 @@ crypto.pbkdf2("secret", "salt", 5000000, 64, "sha512", (err, derivedKey) => {
 //* data structure is very important
 //* naming is very important.Because even in repos like node , developers make mistakes related naming, like they should have been named setImmediate() as process.nextTick() because setImmediate actually happens after oen tick/cycle but they name it setImmediate. and process.nextTick() should be named setImmediate() because it happens after each phrase.Even in node's website they mentioned this mistake, but they can't change it because it will break many people code written before.
 //*There is lot to learn.
+
+//! ⁡⁢⁣⁡⁢⁣⁢⁡⁢⁣⁡⁢⁣⁡⁢⁣⁢Season 1 - Episode - 11 - Creating a server
+//* When we talk about server wh
+/*
+ * What is a Server? The term "server" can refer to both hardware and software, depending on the context.
+ * Hardware
+ *   A physical machine (computer) that provides resources and services to other computers(clients) over a network.
+ * Software
+ *  An application or program that handles requests and delivers data to clients.
+ *
+ * Deploying an Application on a Server When someone says "deploy your app on a server," they usually mean:
+ *     Hardware Aspect: You need a physical machine (server) to run your application. This machine has a CPU, RAM, storage, etc.
+ *      Operating System OS The server hardware runs an operating system like Linux or Windows. Your application runs on this OS.
+ *      Server Software: The software (e.g., a web server like Apache or an application server built with Node.js) that handles requests from users.
+ *
+ *AWS and Cloud Computing AWS Amazon Web Services) provides cloud-based resources, including servers.
+ *   EC2 Instance: When you launch an EC2 instance, youʼre essentially renting a virtual server from AWS. AWS manages the underlying hardware, and you deploy your application on the virtual server.
+ *   Scalability: AWS allows you to easily scale your resources. For example, you can increase the memory or processing power of your server with a few clicks, which is not as straightforward on a physical laptop or desktop.
+ *    Reliability: AWS servers are equipped with constant power, internet backup, and redundant systems to ensure high availability, which is difficult to achieve with a personal computer.*/
+
+//* Can You Use Your Own Laptop as a Server?
+//* A: Yes, but with limitations:Hardware Constraints Your laptop likely has limited RAM, CPU, and storage, which may not be sufficient for handling a large number of requests. Internet Connectivity A home internet connection is typically less reliable and has dynamic IP addresses, making it less suitable for hosting a publicly accessible server.
+//* Power and Maintenance:- Ensuring that your laptop is always on, connected to the internet, and has backup power is challenging. AWS handles all these concerns for you.
+//* Software Servers in Node.js :-When you create an HTTP server in Node.js, you are building an application that listens for requests from clients and responds to them. This is an example of a software server
+
+//* Client-Server Architecture[see image - images\client server architecture.png]
+/*
+ *The term "client" refers to someone accessing a server. Imagine a user sitting at a computer wanting to access a file from a server. For this, the client needs to open a socket connection (not to be confused with WebSocket). Every client has an IP address, and every server has an IP address as well. The client could be a web browser.
+ * To access the file, the client opens a socket connection. On the server side, there should be an application that is listening for such requests, retrieves the requested file, and sends it back to the client
+ * There can be multiple clients, and each client creates a socket connection to get data. After the data is received, the socket connection is closed. If the client needs to make another request, a new socket connection is created, data is retrieved, and the connection is closed again.
+ * Sockets operate using the TCP/IP protocol (Transmission Control Protocol/Internet Protocol).
+ *
+ */
+//? What is a protocol?
+/*
+ *A protocol is a set of rules that define how computers communicate with each other. Protocols determine the format in which data is sent between devices
+ * FTP (File Transfer Protocol): Used for transferring files.
+ * SMTP (Simple Mail Transfer Protocol): Used for sending emails.
+ * HTTP :- When we talk about a web server, we usually mean an HTTP server. HTTP (HyperText Transfer Protocol) is a language or set of rules that defines how clients and servers communicate. Essentially, when we create a server, it's often built using HTTP to handle basic data exchanges. Toward the end, we'll demonstrate how to create a server.
+ */
+//? Q: When you make a server request, how is data sent?
+//* see image - images\data exchanging process between client and web server.png
+//* Data is sent in chunks, and these smaller units are known as packets. Whenever data is transmitted, it's broken down into these packets. Remember the term "packets." The TCP/IP protocol is responsible for sending these packets and ensuring that the data transmission is properly managed. In Node.js, there are concepts of streams and buffers that are used for handling and writing code related to data transmission.Streams is basically how the data is flowing from the server to client and buffer is the process of sending the data in chucks/packets using the stream.
+//*We don't generally communicate using IP addresses; instead, we use domain names like youtube.com. However, at the end of the day, everything maps to an IP address. As humans, we donʼt easily understand or remember IPs, similar to how we save contacts with names instead of memorizing phone numbers. Similarly, when we request a URL like youtube.com, internally, it is translated into an IP address. This is where the Domain Name System DNS comes into play.
+//*A DNS server manages the mapping between domain names and IP addresses. When you request a website, your browser calls a DNS server to resolve the domain name into an IP address. Once the IP is resolved, a call is made to the server.
+//* As a user, you donʼt see what happens behind the scenes. When you request an IP, it goes to the server where an HTTP server processes the request and sends back the video data. This data is sent in chunks, known as streams, and the process involves buffers, which is why you sometimes see the video buffering.
+
+//?  Can I create multiple servers?
+//* see image - images\multiple ports on one server.png
+//*Yes, you can create multiple HTTP servers. Now, suppose a user is sending a request. How do we know which server it should go to? When I mention creating an HTTP server, it means we are setting up two different Node.js applications. The distinction between these servers is defined by a port, which is a 4-digit number (e.g., port 3000). For example, suppose an HTTP server with IP address 102.209.1.3 is running on port 3000 . This combination of IP address and port number ( 102.209.1.3:3000 ) indicates which specific HTTP server the request should be routed to. Essentially, this means thereʼs a single computer (the server) that can run multiple applications, each with its internal servers. The port number determines which application or server the request is directed to
+//* from 11 th page complete work.
